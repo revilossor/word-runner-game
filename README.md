17c8599f88d38eab86cf27ac1e5398f4c4aed24c
Oliver Ross <oliver.ross01@bbc.co.uk>
2020-10-02 21:58:55 +0100

    added assets / game folders

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-..
+TODO
+
+asset pipeline
---
+++ assets/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
+app.use('/images', express.static('images'))
+
+const port = 8000
+
+app.listen(port, () => {
+  console.log(`asset server listening on ${port}`)
+  console.log('\t/images')
+})
